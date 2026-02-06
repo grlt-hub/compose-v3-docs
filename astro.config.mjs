02e2ba406d7185f3c380254c89c3e596f0296316
Viktor Pasynok <v.pasynok@space307.com>
2026-02-06 11:09:38 +0700

    i

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,19 @@ import starlight from "@astrojs/starlight"
 import { defineConfig } from "astro/config"
 import starlightLlmsTxt from "starlight-llms-txt"
 
+const base = '/compose-v3-docs'
+
 export default defineConfig({
   site: "https://grlt-hub.github.io",
-   base: "/compose-v3-docs",
+   base: base,
   integrations: [
     react(),
     starlight({
       title: "App-Compose",
       description: "App Compose - Compose modules into apps",
       social: [
-        { icon: "open-book", label: "Learn", href: "/learn/quick-start/" },
-        { icon: "rocket", label: "Sandbox", href: "/sandbox/" },
+        { icon: "open-book", label: "Learn", href: `${base}/learn/quick-start/` },
+        { icon: "rocket", label: "Sandbox", href: `${base}/sandbox/` },
         { icon: "github", label: "GitHub", href: "https://github.com/grlt-hub/compose-v3-docs" },
       ],
       logo: { src: "./src/assets/logo.svg", replacesTitle: true },
@@ -22,21 +24,21 @@ export default defineConfig({
         {
           label: "Learn",
           items: [
-            { slug: "learn/quick-start" },
-            { slug: "learn/installation" },
-            { slug: "learn/linting" },
-            { slug: "learn/typescript" },
+            { slug: `${base}/learn/quick-start `},
+            { slug: `${base}/learn/installation` },
+            { slug: `${base}/learn/linting` },
+            { slug: `${base}/learn/typescript` },
           ],
           collapsed: true,
         },
         {
           label: "Guides",
           items: [
-            { slug: "guides/coupling-practices" },
-            { slug: "guides/getting-task-result" },
-            { slug: "guides/handling-errors" },
-            { slug: "guides/using-task-status" },
-            { slug: "guides/inspecting-app-topology" },
+            { slug: `${base}/guides/coupling-practices` },
+            { slug: `${base}/guides/getting-task-result` },
+            { slug: `${base}/guides/handling-errors` },
+            { slug: `${base}/guides/using-task-status` },
+            { slug: `${base}/guides/inspecting-app-topology` },
           ],
           collapsed: true,
         },
